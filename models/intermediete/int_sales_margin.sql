9f2842631df4c02f98d211182437d17fb4ad0af8
cankat ergün <arguncankatergun@gmail.com>
2025-09-11 11:03:09 +0000

    Update on int_sales_margin.sql

--- models/intermediete/int_sales_margin.sql
+++ models/intermediete/int_sales_margin.sql
@@ -1,10 +1,33 @@
--- int_sales_margin.sql  (iskelet)
+-- int_sales_margin.sql (iskelet)
+-- öneri: görünürlük için view:
+{{ config(materialized='view') }}
+
 with s_sales as (
-  select * from {{ ref('stg_raw__sales') }}   -- geçici olarak *
+  select
+    orders_id,
+    cast(pdt_id as int64)           as product_id,
+    cast(quantity as int64)         as qty,
+    cast(revenue as numeric)        as revenue,
+    date_date
+  from {{ ref('stg_raw__sales') }}
 ),
 s_product as (
-  select * from {{ ref('stg_raw__product') }} -- geçici olarak *
+  select
+    cast(products_id as int64)      as product_id,
+    cast(purchse_price as numeric)  as purchase_price
+  from {{ ref('stg_raw__product') }}
 )
-select *
-from s_sales
--- join'i birazdan ekleyeceğiz
+
+
+select
+    s.orders_id,
+    s.product_id,
+    s.qty,
+    s.revenue,
+    p.purchase_price,
+    round(s.qty * p.purchase_price, 2) as purchase_cost,
+    round(s.revenue - (s.qty * p.purchase_price), 2) as margin
+from s_sales s
+left join s_product p
+  on s.product_id = p.product_id
+
