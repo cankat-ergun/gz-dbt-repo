67fc37b4bed396930488e01a8567e34496f3ddb8
cankat ergün <arguncankatergun@gmail.com>
2025-09-11 16:18:55 +0000

    added four staging models for ads

---
+++ models/staging/raw/stg_raw__criteo.sql
@@ -0,0 +1,23 @@
+with 
+
+source as (
+
+    select * from {{ source('raw', 'criteo') }}
+
+),
+
+renamed as (
+
+    select
+        date_date,
+        paid_source,
+        campaign_key,
+        campgn_name as campaign_name,
+        cast(ads_cost numeric),
+        impression,
+        click
+    from source
+
+)
+
+select * from renamed
