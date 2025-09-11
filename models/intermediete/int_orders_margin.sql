aeacbaf7a99f5ba9afc2bf5c95177e9f0259c8c9
cankat ergün <arguncankatergun@gmail.com>
2025-09-11 11:46:31 +0000

    Added daily sums per order query to int_orders_margin

--- models/intermediete/int_orders_margin.sql
+++ models/intermediete/int_orders_margin.sql
@@ -3,7 +3,7 @@ SELECT
 	orders_id,
 	date_date,
 	ROUND(SUM(revenue),2) as revenue,
-	ROUND(SUM(quantity),2) as quantity,
+	ROUND(SUM(qty),2) as quantity,
 	ROUND(SUM(purchase_cost),2) as purchase_cost,
 	ROUND(SUM(margin),2) as margin
 FROM {{ ref("int_sales_margin") }}
